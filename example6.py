91cbbb3c55d7f9f3ba9f5a6744bb8c1340affd2a
Woody Huang <woody1573@163.com>
2017-03-02 10:24:04 +0800

    visualization using tensorboard
    Signed-off-by: Woody Huang <woody1573@163.com>

--- example6.py
+++ example6.py
@@ -3,17 +3,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 # This tutorial is about add_layer
 
+layer_count = 1
+
 
 def add_layer(inputs, in_size, out_size, activation_function=None):
-    Weights = tf.Variable(tf.random_uniform([in_size, out_size]))
-    biases = tf.Variable(tf.zeros([1, out_size]) + 0.1)
-    Wx_plus_b = tf.matmul(inputs, Weights) + biases
-    # activate
-    if activation_function is None:
-        outputs = Wx_plus_b
-    else:
-        outputs = activation_function(Wx_plus_b)
-    return outputs
+    global layer_count
+    with tf.name_scope("layer%s" % layer_count):
+        with tf.name_scope("weights"):
+            Weights = tf.Variable(tf.random_uniform([in_size, out_size]), name="W")
+        with tf.name_scope("bias"):
+            biases = tf.Variable(tf.zeros([1, out_size]) + 0.1, name="b")
+        with tf.name_scope("Wx_plus_b"):
+            Wx_plus_b = tf.matmul(inputs, Weights) + biases
+        # activate
+        if activation_function is None:
+            outputs = Wx_plus_b
+        else:
+            outputs = activation_function(Wx_plus_b)
+        layer_count += 1
+        return outputs
 
 
 # Now we build the network
@@ -22,22 +30,29 @@ x_data = np.linspace(-1, 1, 300)[:, np.newaxis]
 noise = np.random.normal(0, 0.05, x_data.shape)
 y_data = np.square(x_data) - 0.5 + noise
 
-xs = tf.placeholder(tf.float32, [None, 1])
-ys = tf.placeholder(tf.float32, [None, 1])
+with tf.name_scope("inputs"):
+    xs = tf.placeholder(tf.float32, [None, 1], name="x_input")
+    ys = tf.placeholder(tf.float32, [None, 1], name="y_input")
 
 l1 = add_layer(xs, 1, 10, activation_function=tf.nn.relu)
 prediction = add_layer(l1, 10, 1, activation_function=None)
 
-loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction), reduction_indices=[1]))
+with tf.name_scope("loss"):
+    loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction), reduction_indices=[1]), name="mean")
 # loss = tf.reduce_mean(tf.square(ys - prediction))
 
-optimizer = tf.train.GradientDescentOptimizer(0.1)
+with tf.name_scope("train"):
+    optimizer = tf.train.GradientDescentOptimizer(0.1)
+    train_step = optimizer.minimize(loss)
 
-train_step = optimizer.minimize(loss)
-
-init = tf.initialize_all_variables()
+with tf.name_scope("www"):
+    init = tf.initialize_all_variables()
 
 sess = tf.Session()
+
+# writer = tf.train.SummaryWriter("logs/", sess.graph_def)
+writer = tf.summary.FileWriter("logs/", sess.graph)
+
 sess.run(init)
 
 fig = plt.figure()
